1ea96d4d57bb4511d3a5db935cdff0f20c05b02d
Vyacheslav <slava93omsk@mail.ru>
2025-02-07 21:13:18 +0600

    001 Подготовка инфраструктуры для сущностей

---
+++ Domain/Abstractions/IEntity.cs
@@ -0,0 +1,11 @@
+namespace Domain.Abstractions;
+
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+
+public interface IEntity
+{
+
+}
---
+++ Domain/ValueObjects/Location.cs
@@ -0,0 +1,21 @@
+namespace Domain.ValueObjects;
+
+public record Location
+{
+    public string City { get; set; } = default!;
+    public string Street { get; set; } = default!;
+
+    private Location(string city, string street)
+    {
+        City = city;
+        Street = street;
+    }
+
+    public static Location Of(string city, string street)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(city);
+        ArgumentException.ThrowIfNullOrEmpty(street);
+
+        return new Location(city, street);
+    }
+}
